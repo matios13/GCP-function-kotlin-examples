05e3528aa97f87b47ce3758f8363f9c6a5f92062
Mateusz Mańka <matios13@gmail.com>
2021-05-16 19:40:56 +0200

    💚 fix buildFunction and runFunction

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.30")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
     implementation("com.google.cloud.functions:functions-framework-api:1.0.4")
+    implementation("com.google.cloud:google-cloud-firestore:2.3.0")
     invoker("com.google.cloud.functions.invoker:java-function-invoker:1.0.2")
 }
 
@@ -45,7 +46,7 @@ task<JavaExec>("runFunction") {
     classpath(invoker)
     inputs.files(configurations.runtimeClasspath, sourceSets["main"].output)
     args(
-        "--target", project.findProperty("runFunction.target") ?:  "$group.App",
+        "--target", project.findProperty("runFunction.target") ?:  "${project.group}.App",
         "--port", project.findProperty("runFunction.port") ?: 8080
     )
     doFirst {
@@ -57,7 +58,7 @@ task("buildFunction") {
     dependsOn("build")
     delete("build/deploy")
     copy {
-        from("build/libs/${rootProject.name}-$version-all.jar")
+        from("build/libs/${project.name}-$version-all.jar")
         into("build/deploy")
     }
 }
